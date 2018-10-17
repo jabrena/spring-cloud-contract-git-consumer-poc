ae8a2469f89b42ef2a25cd1fe714b3ccd10aa475
Juan Antonio Brena Moral <Juan.Brena.Moral@ing.com>
2018-10-17 16:34:49 +0200

    Adding an Integration Tests using Contracts

--- src/main/java/org/jab/microservices/BasicController.java
+++ src/main/java/org/jab/microservices/BasicController.java
@@ -3,7 +3,7 @@ package org.jab.microservices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,28 +17,52 @@ public class BasicController {
 
     private RestTemplate restTemplate;
 
+    private String domain;
+    private int port;
     private String endpoint;
 
     BasicController(
             RestTemplate restTemplate,
-            @Value("${stubs.api.endpoint}") String endpoint) {
+            @Value("${subsystem1.api.domain}") String domain,
+            @Value("${subsystem1.api.port}") int port,
+            @Value("${subsystem1.api.endpoint}") String endpoint) {
 
         this.restTemplate = restTemplate;
+        this.domain = domain;
+        this.port = port;
         this.endpoint = endpoint;
-        LOGGER.info(endpoint);
+
+        LOGGER.info("Subsystem 1: {}:{}{}", domain, port, endpoint);
     }
 
     @GetMapping(path="entrypoint")
     public ConceptReponse getConcept1() throws Exception {
 
-        final String result = getResult(endpoint);
+        final String result = getResult();
         LOGGER.info(result);
 
         return new ConceptReponse("Hello World");
     }
 
-    private String getResult(String endpoint) {
-        ResponseEntity<String> response = restTemplate.getForEntity(endpoint, String.class);
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    private String getResult() {
+
+        final String address = domain + ":" + port + endpoint;
+        LOGGER.info(address);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                address,
+                HttpMethod.GET,
+                new HttpEntity<String>(headers),
+                String.class
+        );
+
         return response.toString();
     }
 
--- src/test/java/org/jab/microservices/IntegrationTests.java
+++ src/test/java/org/jab/microservices/IntegrationTests.java
@@ -1,12 +1,17 @@
 package org.jab.microservices;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,13 +26,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         stubsMode = StubRunnerProperties.StubsMode.REMOTE,
         repositoryRoot = "git://https://github.com/jabrena/spring-cloud-contract-git.git",
         ids = { "org.jab.microservices:spring-cloud-contract-git-producer-poc:0.1.0-SNAPSHOT" })
+
+@DirtiesContext
 public class IntegrationTests {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(IntegrationTests.class);
 
     @Autowired
     private MockMvc mvc;
 
+    @StubRunnerPort("spring-cloud-contract-git-producer-poc") int producerPort;
+
+    @Autowired
+    private BasicController basicController;
+
+    @Before
+    public void setupPort() {
+        basicController.setPort(producerPort);
+    }
+
     @Test
     public void whenGetConcept1_thenReturnOk() throws Exception {
+
+        mvc.perform(get("/entrypoint")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void whenGetConcept1_thenReturnOk_testingDynamicPort() throws Exception {
 
         mvc.perform(get("/entrypoint")
                 .contentType(APPLICATION_JSON))
